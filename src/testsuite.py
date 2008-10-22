8cb0f116df9f27c210bb19f56c6cb4e858003418
bventhur <bventhur@50cd3e91-b052-0410-92d0-9544d30d4854>
2008-10-22 12:16:01 +0000

    * Moved tests/testUdpDecoder to legacy
    git-svn-id: https://pyff.googlecode.com/svn/trunk@78 50cd3e91-b052-0410-92d0-9544d30d4854

--- src/testsuite.py
+++ src/testsuite.py
@@ -17,7 +17,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-import tests.testUdpDecoder
 import tests.testbcixml
 
 import unittest
@@ -30,7 +29,6 @@ logging.basicConfig(level=logging.WARNING,
 
 # Setup the tests
 allTests = unittest.TestSuite()
-#allTests.addTest(tests.testUdpDecoder.suite)
 allTests.addTest(tests.testbcixml.suite)
 
 # Run the tests
