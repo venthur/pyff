8fdf5799126b3ce24624e1d1ad6da01b7f20021c
bventhur <bventhur@50cd3e91-b052-0410-92d0-9544d30d4854>
2009-02-09 17:15:09 +0000

    Made eyetracker robust against disconnections.
    git-svn-id: https://pyff.googlecode.com/svn/trunk@165 50cd3e91-b052-0410-92d0-9544d30d4854

--- src/lib/eyetracker.py
+++ src/lib/eyetracker.py
@@ -24,35 +24,47 @@ class EyeTracker(object):
     
     def __init__(self):
         self.thread = None
-        self.stop = True
+        self.stopping = True
     
     def start(self):
         self.thread = threading.Thread(target=self.listen)
-        self.stop = False
+        self.stopping = False
         self.thread.start()
         
     def stop(self):
-        self.stop = True
+        self.stopping = True
         self.thread.join(10)
         self.thread = None
     
     def listen(self): 
+        print "Preparing socket...",
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind(("", 1111))
         sock.listen(1)
-        conn, addr = sock.accept()
-        while not self.stop:
-            data = conn.recv(1024)
-            print str(data)
-            if data == "REG:EYE":
-                print "Sennding ACK...",
-                conn.send("ACK")
-                print "done."
-        conn.close()
+        print "done."
+        while not self.stopping:
+            print "Waiting for incoming connection...",
+            conn, addr = sock.accept()
+            print "done."
+            while not self.stopping:
+                data = conn.recv(1024)
+                print str(data)
+                if data == "REG:EYE":
+                    print "Sennding ACK...",
+                    conn.send("ACK")
+                    print "done."
+                if not data:
+                    print "Received empty packet, closing connection."
+                    break
+            print "Closing socket...",
+            conn.close()
+            print "done."
 
 
 if __name__ == "__main__":
+    import time
     et = EyeTracker()
     et.start()
+    time.sleep(60)
     # read something
     et.stop()
