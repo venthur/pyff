ac3ea4f694d6c1c0b02a7ca61d805587b2598828
bventhur <bventhur@50cd3e91-b052-0410-92d0-9544d30d4854>
2009-06-05 10:53:34 +0000

    Added stresstest.
    git-svn-id: https://pyff.googlecode.com/svn/trunk@233 50cd3e91-b052-0410-92d0-9544d30d4854

---
+++ src/stresstest.py
@@ -0,0 +1,42 @@
+
+import time
+
+from lib.feedbackcontroller import FeedbackController
+import lib.bcixml
+
+
+play_signal = lib.bcixml.BciSignal(None, [lib.bcixml.CMD_PLAY], lib.bcixml.INTERACTION_SIGNAL)
+    
+    
+
+def main():
+    fc = FeedbackController()
+    feedbacks = fc.fbProcCtrl.get_feedbacks()
+    
+    for fb in feedbacks:
+        print "Starting %s" % fb
+        fc.fbProcCtrl.start_feedback(fb)
+        
+        fc.handle_signal(play_signal)
+        time.sleep(1)
+        
+        print "Stopping %s" % fb
+        fc.fbProcCtrl.stop_feedback()
+        
+        failstop = []
+        try:
+            if fc.fbProcCtrl.currentProc.isAlive():
+                failstop.append(fb)
+        except:
+            pass
+        
+
+    print "Done testing Feedbacks, the following feedbacks failed to stop correctly:"
+    for fb in failstop:
+        print fb
+    
+    
+
+
+if __name__ == "__main__":
+    main()
