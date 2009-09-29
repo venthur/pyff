b738d8b2b5b3a738a05f70db9928e31cc5f4e1af
Bastian Venthur <venthur@debian.org>
2009-09-29 18:59:43 +0200

    Some small cleanups.

--- src/FeedbackBase/PygameFeedback.py
+++ src/FeedbackBase/PygameFeedback.py
@@ -10,15 +10,16 @@ class PygameFeedback(MainloopFeedback):
 
     def init(self):
         self.FPS = 30
-        self.screenPos = [0, 0, 800, 600]
+        self.screenPos = [0, 0]
+        self.screenSizse = [800, 600]
         self.fullscreen = False
         self.caption = "PygameFeedback"
         self.elapsed = 0
-
-        self.backgroundColor = [0,0,0]
+        self.backgroundColor = [0, 0, 0]
         # This is only needed if we need something more than plain colored
         # background (and also for performance)
         self.background = None
+
 
     def pre_mainloop(self):
         self.init_pygame()
@@ -31,58 +32,57 @@ class PygameFeedback(MainloopFeedback):
 
     def tick(self):
         self.process_pygame_events()
-        #pygame.time.wait(10)
         self.elapsed = self.clock.tick(self.FPS)
 
 
     def pause_tick(self):
         pass
 
+
     def play_tick(self):
         pass
-    
+
+
     def init_pygame(self):
         """
         Set up pygame and the screen and the clock.
         """
-        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (self.screenPos[0], 
-                                                        self.screenPos[1])        
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (self.screenPos[0],
+                                                        self.screenPos[1])
         pygame.init()
         pygame.display.set_caption(self.caption)
         if self.fullscreen:
-            self.screen = pygame.display.set_mode((self.screenPos[2], 
-                                                   self.screenPos[3]), 
+            self.screen = pygame.display.set_mode((self.screenPos[2],
+                                                   self.screenPos[3]),
                                                    pygame.FULLSCREEN)
         else:
-            self.screen = pygame.display.set_mode((self.screenPos[2], 
-                                                   self.screenPos[3]), 
+            self.screen = pygame.display.set_mode((self.screenPos[2],
+                                                   self.screenPos[3]),
                                                    pygame.RESIZABLE)
-        self.clock = pygame.time.Clock()    
-        
-    
+        self.clock = pygame.time.Clock()
+
+
     def quit_pygame(self):
         pygame.quit()
-    
-    
+
+
     def do_print(self, text, color, size=None, center=None, superimpose=True):
         """
         Print the given text in the given color and size on the screen.
         """
-        if not color:
-            color = self.fontColor
         if not size:
-            size = self.size/10
+            size = self.size / 10
         if not center:
             center = self.screen.get_rect().center
 
         font = pygame.font.Font(None, size)
         if not superimpose:
             self.screen.blit(self.background, self.backgroundRect)
-        surface = font.render(text, 1, color)    
+        surface = font.render(text, 1, color)
         self.screen.blit(surface, surface.get_rect(center=center))
         pygame.display.flip()
 
-        
+
 
     def init_graphics(self):
         """
@@ -101,7 +101,7 @@ class PygameFeedback(MainloopFeedback):
         """
         for event in pygame.event.get():
             if event.type == pygame.VIDEORESIZE:
-                e = max(event.w, int(round(event.h*0.9)))
+                e = max(event.w, int(round(event.h * 0.9)))
                 self.screen = pygame.display.set_mode((e, event.h), pygame.RESIZABLE)
                 self.resized = True
                 (self.screenHeight, self.screenWidth) = (self.screen.get_height(), self.screen.get_width())
@@ -115,4 +115,3 @@ class PygameFeedback(MainloopFeedback):
                 self.f += step
                 if self.f < -1: self.f = -1
                 if self.f > 1: self.f = 1
-
