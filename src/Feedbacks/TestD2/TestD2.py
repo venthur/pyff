f65d42cf9da563732b813c8bb6eab63f10fc6f51
Bastian Venthur <venthur@debian.org>
2009-10-23 15:18:53 +0200

    Feedback Almost ready, major refactorings.

--- src/Feedbacks/TestD2/TestD2.py
+++ src/Feedbacks/TestD2/TestD2.py
@@ -37,18 +37,26 @@ class TestD2(PygameFeedback):
         self.number_of_rows = 14
         self.seconds_per_row = 20
         self.targets_percent = 45.45
+        # Color of the symbols
+        self.color = [0,0,0]
+        self.backgroundColor = [155, 155, 155]
         # TODO: use unicode instead of pygame stuff (so we cann remove pygame
         # import in the beginning
         self.key_target = pygame.K_1
         self.key_nontarget = pygame.K_2
+
         
     def pre_mainloop(self):
         PygameFeedback.pre_mainloop(self)
         self.generate_rows()
-        
+        self.generate_symbols()
+        # Initialize the logic
         self.elapsed_seconds = 0
         self.current_row = 0
         self.current_col = 0
+        # And here we go...
+        self.present_stimulus()
+
         
     def tick(self):
         PygameFeedback.tick(self)
@@ -63,7 +71,7 @@ class TestD2(PygameFeedback):
                 # TODO: insert final screen and stop feedback.
                 self.on_stop()
             print "Next row."
-            
+            self.present_stimulus()
         #print self.elapsed_seconds
         if self.keypressed:
             self.keypressed = False 
@@ -74,8 +82,7 @@ class TestD2(PygameFeedback):
             if self.current_col > self.items_per_row -1:
                 print "Done with this row."
             else:
-                print self.rows[self.current_row][self.current_col]
-        
+                self.present_stimulus()
 
 
     def generate_rows(self):
@@ -90,7 +97,61 @@ class TestD2(PygameFeedback):
                 [random.choice(NON_TARGETS) for i in range(non_targets)]
             random.shuffle(l)
             self.rows.append(l)
+            
+    
+    def generate_symbols(self):
+        """Generate surfaces all possible symbols."""
+        # Height of the font in pixels
+        fontheight = 200
+        # thickness of the lines (should match thickness of the font
+        linewidth = fontheight / 11
+        
+        font = pygame.font.Font(None, fontheight)
+        surface_d = font.render("d", True, self.color)
+        surface_p = font.render("p", True, self.color)
+        
+        # width and height of letters bounding box
+        width, height = surface_d.get_size()
 
+        # generate the line surfaces
+        surface_l0 = pygame.Surface( (width, height), pygame.SRCALPHA )
+        surface_l1 = pygame.Surface( (width, height), pygame.SRCALPHA )
+        surface_l2 = pygame.Surface( (width, height), pygame.SRCALPHA )
+        pygame.draw.line(surface_l1, self.color, (width/2, height/10), (width/2, height-height/10), linewidth)
+        pygame.draw.line(surface_l2, self.color, (width/3, height/10), (width/3, height-height/10), linewidth)
+        pygame.draw.line(surface_l2, self.color, (2*width/3, height/10), (2*width/3, height-height/10), linewidth)
+
+        self.symbol = {}
+        for symbol in TARGETS + NON_TARGETS:
+            surface = pygame.Surface( (width, height*3), pygame.SRCALPHA )
+            letter = surface_d if symbol[0] == 'd' else surface_p
+            if int(symbol[1]) == 1: 
+                upper = surface_l1
+            elif int(symbol[1]) == 2:
+                upper = surface_l2
+            else:
+                upper = surface_l0
+            if int(symbol[2]) == 1:
+                lower = surface_l1
+            elif int(symbol[2]) == 2:
+                lower = surface_l2
+            else:
+                lower = surface_l0
+            # blit the stuff an pack it in the dict
+            surface.blit(upper, (0, 0))
+            surface.blit(letter, (0, height))
+            surface.blit(lower, (0, 2*height))
+            self.symbol[symbol] = surface
+
+
+    def present_stimulus(self):
+        """Present the current stimulus."""
+        print self.rows[self.current_row][self.current_col]
+       
+        self.screen.fill(self.backgroundColor)
+        symbol = self.rows[self.current_row][self.current_col]
+        self.screen.blit(self.symbol[symbol], self.symbol[symbol].get_rect(center=self.screen.get_rect().center))
+        pygame.display.flip()
             
 
 if __name__ == "__main__":
