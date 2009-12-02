ac53e99bb99d5ad75bef78281429454428f9f3fd
bventhur <bventhur@50cd3e91-b052-0410-92d0-9544d30d4854>
2009-12-02 16:26:45 +0000

    Added description for windows-logger fix
    git-svn-id: https://pyff.googlecode.com/svn/trunk@292 50cd3e91-b052-0410-92d0-9544d30d4854

--- src/lib/feedbackprocesscontroller.py
+++ src/lib/feedbackprocesscontroller.py
@@ -43,6 +43,7 @@ class FeedbackProcess(Process):
 
     def run(self):
         """Run the FeedbackProcess' activities in the new process."""
+        # Setup the logger, since we are in a new process (for Windows only)
         logging.basicConfig(level=self.loglevel, format=self.logformat)
         logging.getLogger("FB").setLevel(self.fbloglevel)
         feedback = self.feedbackClass(port_num=self.port)
