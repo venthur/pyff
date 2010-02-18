db16af5fe12d5cce084cc99a213a40ba1fa299ed
LauraAcqualagna <LauraAcqualagna@1449bcba-80d8-4576-a99e-0f548206d62b>
2010-02-18 15:45:51 +0000

    git-svn-id: https://ml01.zrz.tu-berlin.de/svn/ida/public/bbci/python/pyff@10256 1449bcba-80d8-4576-a99e-0f548206d62b

--- model/sequence_algorithm.py
+++ model/sequence_algorithm.py
@@ -2,25 +2,22 @@ import copy
 import random
 
 class RVSP:
-    
-    matrix=[]
-    
-    
-    def crea_burst(self,burst_list):
-        self.no_lett=list("%*/)?#")
+     
+    def create_burst(self,burst_list):
+        #self.no_lett=list("%*/)?#")
         self.trial = []
         
         for i in range(0,30):
-            self.trial.append(self.no_lett+burst_list[(i*10):(i*10+10)])
+            self.trial.append(burst_list[(i*10):(i*10+10)])
         
         return self.trial
            
         
-    def associa(self,a_list):
+    def link(self,a_list):
         self.alphabet=list("ABCDEFGHIJKLMNOPQRSTUVWXYZ.,:<")
         self.alpharandom=copy.copy(self.alphabet)
         random.shuffle(self.alpharandom)
-        print self.alpharandom
+        #print self.alpharandom
         for k in range(0,30):
             i=0
             while i<len(a_list):
@@ -28,35 +25,28 @@ class RVSP:
                 i+=1
         return a_list
     
-    def canc_elem(self,lista):
+    def del_elem(self,lista):
         for elem in range(0,10):
             del lista[31*elem]
-        print lista
         for elem in range(1,11):
             del lista[30*elem]
         return lista
         
     def random_sequence(self):
         for i in range(1,6):
-            #print "i= ", i
             self.tmp=[]
             for j in range(0,2**(i-1)):
-                #print "j= ", j
                 self.list=range(j,32,2**(i-1))
-                #print self.list
                 self.tmp=self.tmp+self.list
-                #print self.tmp
                 self.rev=copy.copy(self.tmp)
                 self.rev.reverse()
             self.matrix= self.matrix+self.tmp+self.rev
-        #print self.matrix
-        self.new_list=self.canc_elem(self.matrix)
-        #print self.new_list
-        self.ra_list= self.associa(self.new_list)
-        print self.ra_list
+            
+        self.new_list=self.del_elem(self.matrix)
+        self.ra_list= self.link(self.new_list)
+        #print self.ra_list
         self.lungh=len(self.ra_list)
-        #print self.lungh
-        self.trial=self.crea_burst(self.ra_list)
+        self.trial=self.create_burst(self.ra_list)
         print self.trial
 
         
