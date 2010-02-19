2e9dfd80dbe8b6c1373016b6bc171c1b5f74a2c9
LauraAcqualagna <LauraAcqualagna@1449bcba-80d8-4576-a99e-0f548206d62b>
2010-02-19 15:05:57 +0000

    git-svn-id: https://ml01.zrz.tu-berlin.de/svn/ida/public/bbci/python/pyff@10283 1449bcba-80d8-4576-a99e-0f548206d62b

--- model/sequence_algorithm.py
+++ model/sequence_algorithm.py
@@ -5,11 +5,11 @@ import random
 
 class RVSP:
      
-    def create_burst(self,burst_list):
+    def create_burst(self,burst_list,num_seq):
         #self.no_lett=list("%*/)?#")
         self.trial = []
         
-        for i in range(0,30):
+        for i in range(0,3*num_seq):
             self.trial.append(burst_list[(i*10):(i*10+10)])
         
         return self.trial
@@ -27,16 +27,17 @@ class RVSP:
                 i+=1
         return a_list
     
-    def del_elem(self,lista):
-        for elem in range(0,10):
+    def del_elem(self,lista,num_seq):
+        for elem in range(0,num_seq):
             del lista[31*elem]
-        for elem in range(1,11):
+        for elem in range(1,num_seq+1):
             del lista[30*elem]
         return lista
         
-    def random_sequence(self):
+    def random_sequence(self,num_seq):
         self.matrix=[]
-        for i in range(1,6):
+        num=num_seq/2
+        for i in range(1,num_seq):
             self.tmp=[]
             for j in range(0,2**(i-1)):
                 self.list=range(j,32,2**(i-1))
@@ -45,11 +46,11 @@ class RVSP:
                 self.rev.reverse()
             self.matrix= self.matrix+self.tmp+self.rev
             
-        self.new_list=self.del_elem(self.matrix)
+        self.new_list=self.del_elem(self.matrix,num_seq)
         self.ra_list= self.link(self.new_list)
         #print self.ra_list
         self.lungh=len(self.ra_list)
-        self.trial=self.create_burst(self.ra_list)
+        self.trial=self.create_burst(self.ra_list,num_seq)
         print self.trial
 
         
@@ -58,7 +59,8 @@ class RVSP:
         
 if __name__ == "__main__":
     myobject = RVSP()
-    myobject.random_sequence()
+    num_sequences=10
+    myobject.random_sequence(num_sequences)
     
 '''
 Created on 10/feb/2010
