d7ac4cbf8f1cba3b1cc2ca67a198a2c27fce38a0
TorstenSchmits <TorstenSchmits@1449bcba-80d8-4576-a99e-0f548206d62b>
2010-03-08 19:26:07 +0000

    AlphaBurst: do not set screen size in fullscreen mode
    git-svn-id: https://ml01.zrz.tu-berlin.de/svn/ida/public/bbci/python/pyff@10581 1449bcba-80d8-4576-a99e-0f548206d62b

--- view.py
+++ view.py
@@ -27,8 +27,11 @@ from AlphaBurst.util.switcherator import Switcherator
 
 class View(object):
     def __init__(self, width, height, fullscreen, flag):
-        self._screen = Screen(size=(width, height), fullscreen=fullscreen,
-                              sync_swap=True)
+        if fullscreen:
+            self._screen = Screen(fullscreen=fullscreen, sync_swap=True)
+        else:
+            self._screen = Screen(size=(width, height), fullscreen=fullscreen,
+                                sync_swap=True)
         self._flag = flag
         self.__init_attributes()
         self.__init_text()
