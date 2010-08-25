efd9cc79ceceaa18e54a19e1a1fc797537b61f19
tek <tek@moonitor.org>
2010-08-26 00:24:43 +0800

    vision_egg: add main class documentation

--- src/lib/vision_egg/control.py
+++ src/lib/vision_egg/control.py
@@ -32,13 +32,24 @@ VisionEgg.config.VISIONEGG_LOG_TO_STDERR = 0
 VisionEgg.logger.setLevel(logging.ERROR)
 
 class VisionEggControl(MainloopFeedback, Config):
+    """ Main controlling class for VisionEgg feedbacks. It holds and
+    creates the view object, handles keyboard input and provides an
+    interface for creating stimulus sequence objects, that ensure
+    precise presentation timing.
+    The view methods L{add_viewport}, L{add_stimuli} and L{set_stimuli}
+    are forwarded for convenience.
+    """
     def __init__(self, view_type=VisionEggView, *args, **kwargs):
+        """ @param view_type: If a custom view class should be used, its
+        type can be specified here. See _create_view for more.
+        """
         MainloopFeedback.__init__(self, *args, **kwargs)
         Config.__init__(self)
         self._view_type = view_type
         self.__init_attributes()
 
     def __init_attributes(self):
+        """ Setup internal attributes. """
         self._view = self._create_view()
         self.add_viewport = self._view.add_viewport
         self.add_stimuli = self._view.add_stimuli
@@ -48,34 +59,51 @@ class VisionEggControl(MainloopFeedback, Config):
         self.__setup_stim_factory()
 
     def __setup_stim_factory(self):
+        """ Create the factory for stimulus sequence handlers. """
         refresh = VisionEgg.config.VISIONEGG_MONITOR_REFRESH_HZ
         #self._stimseq_fact = StimulusSequenceFactory(1. / refresh)
         self._stimseq_fact = StimulusSequenceFactory(self._flag)
         self._stimseq_fact.set_view(self._view)
 
     def _create_view(self):
+        """ Instantiate the view class. Overload this for custom
+        parameter specification. """
         return self._view_type()
 
     def set_iterator_semaphore(self, flag):
+        """ Specify the object to be used as semaphore for iterators.
+        See L{Switcherator} for more.
+        """
         self._flag = flag
         self._iter = lambda it: Switcherator(flag, it)
         self._view.set_iterator_semaphore(flag)
         
     def __setup_events(self):
+        """ Set L{keyboard_input} to serve as keyboard handler. """
         handlers = [(pygame.KEYDOWN, self.keyboard_input)]
         self._view.set_event_handlers(handlers)
 
     def stimulus_sequence(self, prepare, presentation_time):
         """ Returns an object presenting a series of stimuli.
+        @param prepare: This is the core connection between the sequence
+        handler and user code. It can either be a generator (so
+        presentation continues at a yield statement) or a function
+        (continue upon 'return True'. 'return False' terminates the
+        presentation sequence). The function should prepare the
+        succeeding stimulus.
+        @param presentation_time: The duration of presentation of a
+        single stimulus, in seconds
         """
         return self._stimseq_fact.create(prepare, presentation_time,
                                          self.wait_style_fixed)
 
     def keyboard_input(self, event):
+        """ Handle pygame events like keyboard input. """
         if event.key == pygame.K_q or event.type == pygame.QUIT:
             self.quit()
 
     def pre_mainloop(self):
+        """ Reset the iterator semaphore and initialize the screen. """
         self._flag.reset()
         try:
             self._view.acquire()
@@ -87,6 +115,7 @@ class VisionEggControl(MainloopFeedback, Config):
         self.update_parameters()
 
     def update_parameters(self):
+        """ Apply new parameters set from pyff. """
         params = dict([[p, getattr(self, p, None)] for p in
                         self._view_parameters])
         self._view.update_parameters(**params)
--- src/lib/vision_egg/view.py
+++ src/lib/vision_egg/view.py
@@ -26,32 +26,44 @@ from model.color_word import ColorWord
 from util.switcherator import Flag, Switcherator
 
 class VisionEggView(object):
+    """ This class handles VisionEgg internals and the creation of
+    common/standard stimuli like centered words, a fixation cross or
+    a countdown. Inherit this and pass the type to VisionEggFeedback
+    for customization.
+    """
     def __init__(self):
         self.__init_attributes()
 
     def __init_attributes(self):
+        """ Setup internal attributes. """
         self._logger = logging.getLogger('VisionEggView')
         self._logger.addHandler(logging.FileHandler('log'))
         self._screen_acquired = False
         self._viewports = []
 
     def set_event_handlers(self, event_handlers):
+        """ Set pygame/VisionEgg event handler function. """
         self._event_handlers = event_handlers
 
     def set_iterator_semaphore(self, flag):
+        """ Specify the object to be used as semaphore for iterators.
+        See L{Switcherator} for more.
+        """
         self._iter = lambda it: Switcherator(flag, it)
 
     def update_parameters(self, **kwargs):
+        """ Apply new parameters set from pyff. """
         for k, v in kwargs.iteritems():
             setattr(self, '_' + k, v)
         if self._screen_acquired:
             self.reinit()
 
     def acquire(self):
+        """ Allow L{update_parameters} initialize VisionEgg. """
         self._screen_acquired = True
 
     def reinit(self):
-        self._logger.error('reinit')
+        """ Initialize VisionEgg objects. """
         self.__init_screen()
         self.__init_text()
         self.__init_presentation()
@@ -59,9 +71,17 @@ class VisionEggView(object):
         self.init()
 
     def init(self):
+        """ Overload this for additional custom VisionEgg
+        initialization.
+        """
         pass
 
     def __init_screen(self):
+        """ Create the VisionEgg Screen object using the pyff
+        configuration parameters 'fullscreen' and 'geometry' and the
+        font and background colors according to parameters
+        'font_color_name' and 'bg_color'.
+        """
         params = { 'fullscreen': self._fullscreen, 'sync_swap': True }
         if not self._fullscreen:
             os.environ['SDL_VIDEO_WINDOW_POS'] = '%d, %d' % (self._geometry[0],
@@ -72,31 +92,44 @@ class VisionEggView(object):
         self._set_font_color()
 
     def __init_text(self):
+        """ Provide a text in the screen center for standard stimuli and
+        fixation cross etc.
+        """
         sz = self.screen.size
         self._center_text = ColorWord((sz[0] / 2., sz[1] / 2.),
                                       symbol_size=self._font_size)
 
     def __init_viewports(self):
+        """ Provide a standard viewport. """
         self._standard_viewport = Viewport(screen=self.screen,
                                          stimuli=self._center_text)
         self.add_viewport(self._standard_viewport)
 
     def __init_presentation(self):
+        """ Provide a standard presentation object. """
         self.presentation = Presentation(handle_event_callbacks=
                                          self._event_handlers)
 
     def add_viewport(self, viewport):
+        """ Add an additional custom viewport object to the list of
+        viewports.
+        """
         self._viewports.append(viewport)
         self.presentation.set(viewports=self._viewports)
 
     def add_stimuli(self, *stimuli):
+        """ Add additional custom stimulus objects to the list of
+        stimuli.
+        """
         stimuli = self._standard_viewport.parameters.stimuli + list(stimuli)
         self.set_stimuli(*stimuli)
 
     def set_stimuli(self, *stimuli):
+        """ Set the list of stimulus objects.  """
         self._standard_viewport.set(stimuli=stimuli)
 
     def _set_font_color(self):
+        """ Set the standard font color by pygame name. """
         try:
             self._font_color = Color(self._font_color_name).normalize()
         except ValueError:
@@ -104,35 +137,44 @@ class VisionEggView(object):
                               str(self._font_color_name))
 
     def _set_bg_color(self):
+        """ Set the standard background color by pygame name. """
         try:
-            self._logger.error('bg before')
             self.screen.set(bgcolor=Color(self._bg_color).normalize())
-            self._logger.error('bg after')
         except ValueError:
             self._logger.warn('No such pygame.Color: %s' % str(self._bg_color))
 
     def present_frames(self, num_frames):
+        """ Launch the presentation main loop for a given number of
+        frames.
+        """
         self.presentation.set(go_duration=(num_frames, 'frames'))
         self.presentation.go()
 
     def _center_word(self, text, color=None):
+        """ Set the standard word in the screen center. """
         self._center_text.set(text=text)
         self._center_text.set(colors=color or (self._font_color for l in
                                            self._center_text))
 
     def ask(self):
+        """ Display a question mark in the center and wait for keyboard
+        input. The query is terminated by calling L{answered}.
+        """
         self._center_word('?')
         self._asking = True
         self.presentation.run_forever()
         self.presentation.set(quit=False)
 
     def answered(self):
-        """ Abort the current presentation (normally the question mark)
+        """ Abort the current presentation (usually the question mark)
         after subject input.
         """
         self.presentation.set(quit=True)
 
     def show_fixation_cross(self):
+        """ Display a plus sign as fixation cross for the period of time
+        given by pyff parameter 'fixation_cross_time'.
+        """
         self._center_word('+')
         self._present(self._fixation_cross_time)
 
@@ -140,10 +182,13 @@ class VisionEggView(object):
         """ Remove the stimulus from the screen. Alternatives welcome.
         """
         self._center_text.set_all(on=False)
-        self._present(0.0000000001)
+        self.present_frames(1)
         self._center_text.set_all(on=True)
 
     def count_down(self):
+        """ Display a countdown according to pyff parameters
+        'count_down_start' and 'count_down_symbol_duration'.
+        """
         for i in self._iter(reversed(xrange(self._count_down_start + 1))):
             self._center_word(str(i))
             self._present(self._count_down_symbol_duration)
@@ -155,4 +200,5 @@ class VisionEggView(object):
         self.screen.close()
 
     def quit(self):
+        """ Stop the presentation. """
         self.presentation.set(quit=True)
