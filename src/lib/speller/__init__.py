f5afee7d9f70b7b8c3521b658befda5ffa090901
tek <tek@gentoo64.net>
2011-02-24 02:06:05 +0100

    speller: add decorator for stimulus generator function using stim time yielding

--- src/lib/speller/__init__.py
+++ src/lib/speller/__init__.py
@@ -23,6 +23,7 @@ from lib.speller.experiment import *
 class Speller(object):
     __stimulus = None
     __sequences = None
+    __stim_gen = None
 
     def __init__(self):
         self.__init_attributes()
@@ -57,6 +58,14 @@ class Speller(object):
         return f
 
     @classmethod
+    def stimulus_generator(self, **kw):
+        def decorate(f):
+            self.__stim_gen = f
+            return f
+        self.__stim_gen_kw = kw
+        return decorate
+
+    @classmethod
     def sequences(self, f):
         self.__sequences = f
         return f
@@ -72,6 +81,9 @@ class Speller(object):
                                        self)
         if self.__stimulus:
             self._trial._sequence = getattr(self, self.__stimulus.__name__)
+        elif self.__stim_gen:
+            self._trial._sequence = self._stimulus_generator
+            self.__stimulus_generator = getattr(self, self.__stim_gen.__name__)
 
     def _setup_input_handler(self):
         input_handler_type = self._trial_name + 'InputHandler'
@@ -104,3 +116,7 @@ class Speller(object):
         cls = data.get('cl_output', None)
         if cls is not None:
             self._input_handler.eeg_select(cls)
+
+    def _stimulus_generator(self, *a, **kw):
+        self.stimulus_sequence(self.__stimulus_generator(*a, **kw),
+                               **self.__stim_gen_kw).run()
