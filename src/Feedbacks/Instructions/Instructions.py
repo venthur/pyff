d559c6e227ecc653169a20ddafa823a9f4ad71e7
Bastian Venthur <venthur@debian.org>
2011-09-27 16:33:00 +0200

    Reformatted the code a bit.

--- src/Feedbacks/Instructions/Instructions.py
+++ src/Feedbacks/Instructions/Instructions.py
@@ -1,64 +1,57 @@
-# Instructions.py 
+# Instructions.py
 # Author: Lovisa Irpa Helgadottir
 
+
 import subprocess as sp
-    
 import time, sys, os
 
 from FeedbackBase.MainloopFeedback import MainloopFeedback
 
+
 class Instructions(MainloopFeedback):
-    '''
-        Instruction Feedback. 
-        
-        This class opens up a html file or and url in browser and closes it after predetermined time or after a key press. 
-        
-        Default browser is firefox. 
-        
-        ### Windows ### 
-        Browsers: 
-        - Mozilla Firefox
-        - Google Chrome
-        - Internet Explorer
-        - Opera
-        
+    '''Instruction Feedback.
 
-        ### LINUX ###
-        Any browser can be used. 
-        The self.browser variable should be the command used to start the browser from a command line. 
-        e.g. to start chrome:
-        self.browser = 'chromium-browser' 
+    This class opens up a html file or and url in browser and closes it after
+    predetermined time or after a key press.
 
+    Default browser is firefox.
+
+    ### Windows ###
+    Browsers:
+    - Mozilla Firefox
+    - Google Chrome
+    - Internet Explorer
+    - Opera
+
+
+    ### LINUX ###
+    Any browser can be used.
+    The self.browser variable should be the command used to start the browser
+    from a command line.  e.g. to start chrome:
+    self.browser = 'chromium-browser'
     '''
 
     def init(self):
-        ''' 
-             Initialize parameters
-        '''
+        '''Initialize parameters.'''
         self.urlname='' # Html file or url
         self.browser='firefox' #default firefox
         self.duration=10 #s
-     
-        # The browser can either be closed after a certain amount of time or with a keypress
-        close_opts=['time','keypress']  
+        # The browser can either be closed after a certain amount of time or
+        # with a keypress
+        close_opts=['time','keypress']
         self.close=close_opts[1] #default 'key'
-        
-      
-           
-    def tick(self):
 
+
+    def tick(self):
         if sys.platform =='win32':
             self.win_tick()
         else:
             self.linux_tick()
-            
         self._running = False
 
 
     def win_tick(self):
-
        dir_opts=['Mozilla Firefox/','Google/Chrome/Application/','Internet Explorer/','Opera/']
-
        if self.browser == 'firefox':
            directory=dir_opts[0]
        else:
@@ -73,17 +66,14 @@ class Instructions(MainloopFeedback):
                    else:
                        print("Browser Not Defined \n ... Quitting ...")
                        sys.exit()
-
-
        if (os.path.isdir("C:/Program Files/"+directory)):
-           instructions=sp.Popen([r"C:\Program Files/"+directory+self.browser+".exe",self.urlname])                
+           instructions=sp.Popen([r"C:\Program Files/"+directory+self.browser+".exe",self.urlname])
        else:
            if (os.path.isdir("C:/Program Files (x86)/"+directory)):
                instructions=sp.Popen([r"C:/Program Files (x86)/"+directory+self.browser+".exe",self.urlname])
            else:
                print('Browser Directory Not Found \n ... Quitting ...')
                sys.exit()
-                
        if (self.close=='time'):
            time.sleep(self.duration)
            instructions.kill()
@@ -93,27 +83,19 @@ class Instructions(MainloopFeedback):
 
 
     def linux_tick(self):
-
-        instructions = sp.Popen([self.browser,self.urlname])	
-
+        instructions = sp.Popen([self.browser,self.urlname])
         if (self.close=='time'):
             time.sleep(self.duration)
             instructions.kill()
         else:
             raw_input()
             instructions.kill()
-              
+
+
 if __name__ == '__main__':
     inst = Instructions()
     inst.on_init()
     inst.urlname='www.tu-berlin.de'
     inst.browser='chrome'
-    inst.on_play()      
-        
- 
+    inst.on_play()
 
-
-
-
- 
-   
