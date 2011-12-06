ccd6cc23bd12bc2ada49a6d1e7a4afcd05d437a6
tek <tek@gentoo64.net>
2011-12-06 02:20:05 +0100

    vision_egg: Add extra parameter for resolution in fullscreen mode

--- src/lib/vision_egg/view.py
+++ src/lib/vision_egg/view.py
@@ -94,9 +94,11 @@ class VisionEggView(object):
         'font_color_name' and 'bg_color'.
         """
         params = { 'fullscreen': self._fullscreen, 'sync_swap': True }
-        if not self._fullscreen:
+        if self._fullscreen:
+            params['size'] = self._fullscreen_resolution
+        else:
             os.environ['SDL_VIDEO_WINDOW_POS'] = '%d, %d' % (self._geometry[0],
-                                                            self._geometry[1])
+                                                             self._geometry[1])
             params['size'] = self._geometry[2:]
         self.screen = Screen(**params)
         self._set_bg_color()
