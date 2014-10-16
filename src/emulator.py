ba18516d368fba2b5c583208adc942a26660ff8c
Bastian Venthur <venthur@debian.org>
2014-10-16 16:46:07 +0200

    Fixed some whitespaces.

--- src/emulator.py
+++ src/emulator.py
@@ -32,10 +32,10 @@ from lib.bcixml import BciSignal
 
 
 class Emulator(cmd.Cmd):
-    
+
     prompt = "> "
     intro = "Welcome to the BCI emulator. Type help to see a list of available commands."
-    
+
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.stopping = True
@@ -43,7 +43,7 @@ class Emulator(cmd.Cmd):
     def do_quit(self, line):
         """Quit the Emulator."""
         return True
-    
+
     def postloop(self):
         print
 
@@ -71,7 +71,7 @@ class Emulator(cmd.Cmd):
         """Generates a control signal and sends it to the Feedback Controller."""
         self._do_generate_cs(line, 6)
 
-        
+
     def _do_generate_cs(self, line, numbers):
         self.net = BciNetwork("localhost", bcinetwork.FC_PORT)
         self.signal = BciSignal(None, None, bcixml.CONTROL_SIGNAL)
@@ -80,7 +80,7 @@ class Emulator(cmd.Cmd):
         self.t.start()
         print "Enter: stop_cs to stop the signal."
 
-        
+
     def _cs_loop(self, numbers=1):
         c = 0
         while not self.stopping:
@@ -93,30 +93,30 @@ class Emulator(cmd.Cmd):
             sample4 = math.sin(r-270.0)
             sample5 = math.sin(r-45.0)
             sample6 = math.sin(r-135.0)
-            
+
             samples = [sample1, sample2, sample3, sample4, sample5, sample6]
 
             #self.signal.data = {"data" : [sample1, sample2, sample3]}
-            if numbers == 1: 
+            if numbers == 1:
                 self.signal.data = {"cl_output" : samples[0]}
             elif numbers <= 6:
                 self.signal.data = {"cl_output" : samples[:numbers]}
             else:
                 print "Error don't know how to handle %i numbers." % numbers
-                
+
             self.net.send_signal(self.signal)
 
     def do_stop_cs(self, line):
         """Stops the loop which sends data to the Feedback Controller."""
         self.stopping = True
         self.t.join()
-        
+
     def do_status(self, line):
         """Get the status of the emulator."""
         for var, val in self.__dict__.iteritems():
             print str(var), str(val)
-        
-        
+
+
 
 if __name__ == "__main__":
     Emulator().cmdloop()
